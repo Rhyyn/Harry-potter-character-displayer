8a07454755364bf195d04359eb4e84550af933de
Rhyn <rhyyn.lol@gmail.com>
2021-11-15 16:22:43 +0100

    Finally made the search bar work

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import CharacterCard from "./components/CharacterCard";
-// import { json } from "express";
+import SearchBox from "./components/SearchBox";
 
-//--openssl-legacy-provider start
 
 function App() {
   const [data, setData] = useState(null);
+  const [search, setSearch] = useState("");
+
+// fetch data from API, sort the data then set the data as a State
   useEffect(() => {
     fetch("https://hp-api-rhyn.herokuapp.com/characters")
       .then((res) => {
@@ -21,16 +23,27 @@ function App() {
       });
   }, []);
 
-  // console.log(data.Items[6]);
+
+// set the user search text to a State then filter data to find whether it matches the userSearch, if yes display all card that matches
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  let searchData = data.Items.filter((item) => {
+      return Object.keys(item).some(key => 
+        item[key].toString().toLowerCase().includes(search.toString().toLowerCase()))
+  });
+
+
 
   return (
     <div className="App">
       <header>
         <h1>Harry Potter Characters</h1>
       </header>
+      <SearchBox handleChange={handleChange.bind(this)} value={search} />
       <div className="cardBox">
         {data &&
-          data.Items.map((item) => (
+          searchData.map((item) => (
             <CharacterCard
               key={item.id}
               charName={item.name}

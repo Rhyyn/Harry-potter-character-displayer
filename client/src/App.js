193dbe527b6d04514495da2f0410e9a5c11e7ca6
Rhyn <rhyyn.lol@gmail.com>
2021-12-01 12:09:05 +0100

    Refactored React Tree

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
-import MainHeader from "./components/MainHeader";
+import Houses from "./routes/Houses";
+import CharactersPage from "./routes/CharactersPage";
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Students from "./routes/Students";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchMyApi() {
+      fetch("http://hp-api.herokuapp.com/api/characters")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          data.sort(compare);
+          function compare(a, b) {
+            return a.id - b.id;
+          }
+          setData(data);
+        });
+    }
+    fetchMyApi();
+  }, []);
 
   return (
     <div className="App">
-    <MainHeader />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="characters" element={<CharactersPage data={data}/>}></Route>
+        <Route path="houses" element={<Houses />}></Route>
+        <Route path="students" element={<Students data={data} />}></Route>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CharactersPage from "./routes/CharactersPage";
-import Houses from "./routes/Houses"
+import { BrowserRouter } from "react-router-dom";
+// import CharactersPage from "./routes/CharactersPage";
+// import Houses from "./routes/Houses"
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        {" "}
-      </Route>
-      <Route path="characters" element={<CharactersPage />}></Route>
-      <Route path="houses" element={<Houses />}></Route>
-    </Routes>
+      <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();

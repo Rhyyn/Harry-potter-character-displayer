0e28bbd4d162604b886c1857a8e454b4b6c71711
Rhyn <rhyyn.lol@gmail.com>
2021-12-17 13:49:58 +0100

    Button to bring back user to the top of the page

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,9 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import ReorderIcon from '@mui/icons-material/Reorder';
+import ReorderIcon from "@mui/icons-material/Reorder";
+import ButtonTopScroll from "../components/ButtonTopScroll";
 
 const MainHeader = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const [showButtn, setShowButtn] = useState(true);
+  const [buttonClassName, setButtonClassName] = useState("");
+  const [buttonId, setButtonId] = useState("");
+
+  window.onscroll = function () { // on user Scroll scrollFunction()
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 900) {  // if user has scrolled past 900px show button
+      setShowButtn(!showButtn);
+      setButtonClassName("button-top-scroll");
+      setButtonId("");
+    } else if (document.documentElement.scrollTop < 900) { // else if user has not scrolled past 900px hide button
+      setShowButtn(false);
+      setButtonId("hideButton");
+      setButtonClassName("");
+    }
+  }
+
+  function topFunction() {  // on user Click go back to the top of the page
+    document.body.scrollTop = 0;   
+    document.documentElement.scrollTop = 0;
+  }
+
   return (
     <header>
       <div className="navBarContainer">
@@ -32,8 +58,18 @@ const MainHeader = () => {
             </Link>
           </nav>
         </div>
-
-        <button onClick={() => setShowLinks(!showLinks)} className="hamburgerButton"><ReorderIcon /></button>
+        <div className="button-scroll-container" onClick={() => topFunction()}> 
+          <ButtonTopScroll
+            className={buttonClassName}
+            id={buttonId}
+          />
+        </div>
+        <button
+          onClick={() => setShowLinks(!showLinks)}
+          className="hamburgerButton"
+        >
+          <ReorderIcon />
+        </button>
       </div>
     </header>
   );

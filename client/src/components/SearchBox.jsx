7418123364f2220390f8dd92c20b083e8f0781c7
Rhyn <rhyyn.lol@gmail.com>
2021-12-21 13:36:07 +0100

    Spellcheck false

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -7,6 +7,7 @@ const SearchBox = (props) => {
         className="search-bar"
         placeholder="Search.."
         type="text"
+        spellcheck="false"
         onChange={props.handleChange}
       ></input>
     </div>

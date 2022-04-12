5e3c2f23fe3edecac327f20bb8606aa8c621cf92
Rhyn <rhyyn.lol@gmail.com>
2022-04-12 17:40:56 +0200

    Houses Images not displaying correctly fix

--- client/src/routes/Houses.jsx
+++ client/src/routes/Houses.jsx
@@ -3,38 +3,21 @@ import MainHeader from "../components/MainHeader";
 import { Link } from "react-router-dom";
 
 const Houses = () => {
-  // const handleHouseRoute = () => {
-
-  // }
-
   return (
     <div>
-      <div>
-        <MainHeader />
-      </div>
-      <div className="container-container">
-        <div className="houses-container">
-          <Link to="/houses/gryffindor">
-            <div className="houses gryffindor">
-              <h2>Gryffindor</h2>
-            </div>
-          </Link>
-          <Link to="/houses/slytherin">
-            <div className="houses slytherin">
-              <h2>Slytherin</h2>
-            </div>
-          </Link>
-          <Link to="/houses/hufflepuff">
-            {" "}
-            <div className="houses hufflepuff">
-              <h2>Hufflepuff</h2>
-            </div>
-          </Link>
-          <Link to="/houses/ravenclaw">
-            <div className="houses ravenclaw">
-              <h2>Ravenclaw</h2>
-            </div>
-          </Link>
+      <MainHeader />
+      <div className="houses-container">
+        <div className="gryffindor">
+          <Link className="gryffindor-image" to="/houses/gryffindor"></Link>
+        </div>
+        <div className="slytherin">
+          <Link className="slytherin-image" to="/houses/gryffindor"></Link>
+        </div>
+        <div className="hufflepuff">
+          <Link className="hufflepuff-image" to="/houses/gryffindor"></Link>
+        </div>
+        <div className="ravenclaw">
+          <Link className="ravenclaw-image" to="/houses/gryffindor"></Link>
         </div>
       </div>
     </div>

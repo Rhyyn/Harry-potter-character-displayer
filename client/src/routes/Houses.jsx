96d99168eef37d5a803877f225d1950fa0656400
Rhyn <rhyyn.lol@gmail.com>
2022-04-14 15:47:31 +0200

    Removed Houses route

--- client/src/routes/Houses.jsx
+++ client/src/routes/Houses.jsx
@@ -6,20 +6,6 @@ const Houses = () => {
   return (
     <div>
       <MainHeader />
-      <div className="houses-container">
-        <div className="gryffindor">
-          <Link className="gryffindor-image" to="/houses/gryffindor"></Link>
-        </div>
-        <div className="slytherin">
-          <Link className="slytherin-image" to="/houses/gryffindor"></Link>
-        </div>
-        <div className="hufflepuff">
-          <Link className="hufflepuff-image" to="/houses/gryffindor"></Link>
-        </div>
-        <div className="ravenclaw">
-          <Link className="ravenclaw-image" to="/houses/gryffindor"></Link>
-        </div>
-      </div>
     </div>
   );
 };

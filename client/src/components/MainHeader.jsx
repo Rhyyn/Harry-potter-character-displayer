037b24465fb8fa2b4d6c3c6e9ab8497d47f0e4fa
Rhyn <rhyyn.lol@gmail.com>
2022-05-25 17:43:11 +0200

    Fix: indentation

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -16,152 +16,190 @@ import { useState } from "react";
 const pages = ["Characters", "Houses", "Staff", "Students"];
 
 const MainHeader = (props) => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [showHouseDrop, setShowHouseDrop] = useState(false);
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [showHouseDrop, setShowHouseDrop] = useState(false);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleHouseMenu = () => {
-    handleCloseNavMenu();
-    setShowHouseDrop(!showHouseDrop);
-  };
+    const handleHouseMenu = () => {
+        handleCloseNavMenu();
+        setShowHouseDrop(!showHouseDrop);
+    };
 
-  const search = () => {
+    const search = () => {
+        return (
+            <SearchBox
+                handleChange={props.handleChange}
+                value={props.value}
+            ></SearchBox>
+        );
+    };
+
     return (
-      <SearchBox
-        handleChange={props.handleChange}
-        value={props.value}
-      ></SearchBox>
-    );
-  };
+        <AppBar position="static">
+            <Container maxWidth="xl" sx={{ fontFamily: "Montserrat" }}>
+                <Toolbar disableGutters>
+                    <Typography
+                        className="text-link brandName"
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "Montserrat",
+                        }}
+                    >
+                        HP Characters
+                    </Typography>
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl" sx={{fontFamily: 'Montserrat'}}>
-        <Toolbar disableGutters >
-          <Typography
-            className="text-link brandName"
-            variant="h6"
-            noWrap
-            component={Link}
-            to="/"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" }, fontFamily: 'Montserrat' }}
-          >
-            HP Characters
-          </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    className="hamburgerContainer"
+                                >
+                                    <Typography
+                                        sx={{
+                                            color: "black",
+                                            fontFamily: "Montserrat",
+                                        }}
+                                        className="text-link"
+                                        component={Link}
+                                        to={"/" + page}
+                                        textAlign="center"
+                                    >
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }}}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  className="hamburgerContainer"
-                >
-                  <Typography
-                    sx={{ color: "black", fontFamily: 'Montserrat' }}
-                    className="text-link"
-                    component={Link}
-                    to={"/" + page}
-                    textAlign="center"
-                  >
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Typography
-            variant="h6"
-            className="text-link brandName"
-            noWrap
-            component={Link}
-            to="/"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-          >
-            HP Characters
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page, index) => {
-              if (page !== "Houses") {
-                return (
-                  <Button
-                    key={page}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: "white", display: "block", fontFamily: 'Montserrat' }}
-                    component={Link}
-                    to={"/" + page.toLowerCase()}
-                  >
-                    {page}
-                  </Button>
-                );
-              } else if (page === "Houses") {
-                return (
-                  <Button
-                    key={page}
-                    onClick={handleHouseMenu}
-                    sx={{ my: 2, color: "white", display: "block", fontFamily: 'Montserrat'  }}
-                  >
-                    {page}
-                    {showHouseDrop ? (
-                      <div className="dropdownSubMenu">
-                        <ul className="dropdownSubMenuList">
-                          <li className="dropdownSubMenuItems">
-                            <Link to="/houses/gryffindor">Gryffindor</Link>
-                          </li>
-                          <li className="dropdownSubMenuItems">
-                            <Link to="/houses/slytherin">Slytherin</Link>
-                          </li>
-                          <li className="dropdownSubMenuItems">
-                            <Link to="/houses/ravenclaw">Ravenclaw</Link>
-                          </li>
-                          <li className="dropdownSubMenuItems">
-                            <Link to="/houses/hufflepuff">Hufflepuff</Link>
-                          </li>
-                        </ul>
-                      </div>
-                    ) : null}
-                  </Button>
-                );
-              } else {
-                return null;
-              }
-            })}
-            {/* {pages.map((page) => (
+                    <Typography
+                        variant="h6"
+                        className="text-link brandName"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
+                        HP Characters
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        {pages.map((page, index) => {
+                            if (page !== "Houses") {
+                                return (
+                                    <Button
+                                        key={page}
+                                        onClick={handleCloseNavMenu}
+                                        sx={{
+                                            my: 2,
+                                            color: "white",
+                                            display: "block",
+                                            fontFamily: "Montserrat",
+                                        }}
+                                        component={Link}
+                                        to={"/" + page.toLowerCase()}
+                                    >
+                                        {page}
+                                    </Button>
+                                );
+                            } else if (page === "Houses") {
+                                return (
+                                    <Button
+                                        key={page}
+                                        onClick={handleHouseMenu}
+                                        sx={{
+                                            my: 2,
+                                            color: "white",
+                                            display: "block",
+                                            fontFamily: "Montserrat",
+                                        }}
+                                    >
+                                        {page}
+                                        {showHouseDrop ? (
+                                            <div className="dropdownSubMenu">
+                                                <ul className="dropdownSubMenuList">
+                                                    <li className="dropdownSubMenuItems">
+                                                        <Link to="/houses/gryffindor">
+                                                            Gryffindor
+                                                        </Link>
+                                                    </li>
+                                                    <li className="dropdownSubMenuItems">
+                                                        <Link to="/houses/slytherin">
+                                                            Slytherin
+                                                        </Link>
+                                                    </li>
+                                                    <li className="dropdownSubMenuItems">
+                                                        <Link to="/houses/ravenclaw">
+                                                            Ravenclaw
+                                                        </Link>
+                                                    </li>
+                                                    <li className="dropdownSubMenuItems">
+                                                        <Link to="/houses/hufflepuff">
+                                                            Hufflepuff
+                                                        </Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        ) : null}
+                                    </Button>
+                                );
+                            } else {
+                                return null;
+                            }
+                        })}
+                        {/* {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -172,11 +210,11 @@ const MainHeader = (props) => {
                 {page}
               </Button>
             ))} */}
-          </Box>
-          <div className="search-box-DIV">{search()}</div>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    </Box>
+                    <div className="search-box-DIV">{search()}</div>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
 export default MainHeader;

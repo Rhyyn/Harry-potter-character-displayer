79c318255426620a74ca3189f2e7d0082cbde554
Rhyn <rhyyn.lol@gmail.com>
2022-05-25 17:42:51 +0200

    Styling: Start of home styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,24 +1,43 @@
+import { Box } from "@mui/material";
 import React from "react";
 import MainHeader from "./MainHeader";
+import castle from '../images/homeHogwart.jpg'; 
 const Home = () => {
-  return (
-    <div className="home-container">
-      <MainHeader />
-      <h1 className="home-h1">Welcome to Harry Potter Character Displayer</h1>
-      <div className="credits-container">
-        <h2 className="credits">Credits</h2>
-        <p className="credits-desc">
-          <a href="https://hp-api.herokuapp.com/" className="text-link">
-            API from : https://hp-api.herokuapp.com/
-          </a>
-        </p>
-      </div>
-      <div className="contact-container">
-        <h2 className="contact">Contact</h2>
-        <p className="contact-desc">tonymigeonpro@gmail.com</p>
-      </div>
-    </div>
-  );
+    return (
+        <section className="homeContainer">
+            <MainHeader />
+            <Box
+                sx={{
+                    height: "calc(100vh - 68.5px)",
+                    display: "flex",
+                }}
+            >
+                <Box sx={{ width: "25%", display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <h1 className="homeTitle">
+                        Welcome to Harry Potter Character Displayer
+                    </h1>
+                    <Box className="creditsContainer">
+                        <h2 className="credits">Credits</h2>
+                        <p className="creditsDesc">
+                            <a
+                                href="https://hp-api.herokuapp.com/"
+                                className="textLink"
+                            >
+                                API from : https://hp-api.herokuapp.com/
+                            </a>
+                        </p>
+                    </Box>
+                    <Box className="contactContainer">
+                        <h2 className="contact">Contact</h2>
+                        <p className="contactDesc">tonymigeonpro@gmail.com</p>
+                    </Box>
+                </Box>
+                <Box sx={{ width: "50%" }}>
+                    <img className="homeImage" src={castle} />
+                </Box>
+            </Box>
+        </section>
+    );
 };
 
 export default Home;

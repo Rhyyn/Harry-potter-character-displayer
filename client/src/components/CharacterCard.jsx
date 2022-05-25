ab5810a5408e8992ce80b1b50a540ea0de175679
Rhyn <rhyyn.lol@gmail.com>
2022-05-25 11:46:21 +0200

    Styling : New Card styling

--- client/src/components/CharacterCard.jsx
+++ client/src/components/CharacterCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardContent from '@mui/material/CardContent';
+import CardContent from "@mui/material/CardContent";
 import { Box } from "@mui/material";
 
 const CharacterCard = ({
@@ -20,29 +20,63 @@ const CharacterCard = ({
   }
   let url = "http://localhost:3000/img/";
 
+  function titleCase(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase(); // first letter to UpperCase
+  }
+
   return (
-    <Card className="card" sx={{width: '300px', bgcolor: 'primary.main', display: "flex", justifyContent: "center", margin: "1rem", flexShrink: '0' }}>
-      <CardContent sx={{display: 'flex', justifyContent: 'center', flexDirection: 'column', textAlign: 'center', alignItems: 'center', color: 'white'}}>
+    <Card
+      className="card"
+      sx={{
+        minWidth: "17rem",
+        maxWidth: "17rem",
+        minHeight: "330px",
+        bgcolor: "primary.main",
+        display: "flex",
+        justifyContent: "center",
+        margin: "1rem",
+      }}
+    >
+      <CardContent
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
         <h2 className="cardCharName">{charName}</h2>
-        <Box component="img" sx={{maxWidth: '5rem'}} src={charImage} alt={charName}></Box>
+        <Box
+          component="img"
+          sx={{ maxHeight: "7rem", borderRadius: "8px", margin: "1rem" }}
+          src={charImage}
+          alt={charName}
+        ></Box>
         <div className="cardDesc">
           <p className="cardCharYearOfBirth">
-            Year of Birth: {charYearOfBirth ? charYearOfBirth : "Unknown"}
+            Born in: {charYearOfBirth ? charYearOfBirth : "Unknown"}
           </p>
           <p className="cardCharAncestry">
-            Blood Status: {charAncestry ? charAncestry : "Unknown"}
+            Blood Status: {charAncestry ? titleCase(charAncestry) : "Unknown"}
           </p>
           <p className="cardCharHouse">
             House: {charHouse ? charHouse : "Unknown"}
           </p>
-          <Box component="img" sx={{maxWidth: '5rem'}} src={url + currentHouse} alt="temp"></Box>
+          <Box
+            component="img"
+            sx={{ maxWidth: "5rem", marginTop: '1rem' }}
+            src={url + currentHouse}
+            alt="temp"
+          ></Box>
         </div>
       </CardContent>
     </Card>
   );
 };
 
-  /* <div className="uniCard">
+/* <div className="uniCard">
   <h2 className="cardCharName">{charName}</h2>
   <img className="cardCharImage" src={charImage} alt={charName} />
   <div className="cardDesc">
@@ -57,7 +91,4 @@ const CharacterCard = ({
   </div>
 </div>; */
 
-
 export default CharacterCard;
-
-

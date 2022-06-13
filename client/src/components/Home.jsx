38b55ff5661dc819ef83c1b93f2e6acb61e91eee
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-13 17:32:38 +0200

    Removed home styling for new upcoming styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 import MainHeader from "./MainHeader";
 import castle from '../images/homeHogwart.jpg'; 
@@ -10,30 +10,12 @@ const Home = () => {
                 sx={{
                     height: "calc(100vh - 68.5px)",
                     display: "flex",
+                    backgroundImage: "url('homeHogwart.jpg')"
                 }}
             >
-                <Box sx={{ width: "25%", display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                    <h1 className="homeTitle">
-                        Welcome to Harry Potter Character Displayer
-                    </h1>
-                    <Box className="creditsContainer">
-                        <h2 className="credits">Credits</h2>
-                        <p className="creditsDesc">
-                            <a
-                                href="https://hp-api.herokuapp.com/"
-                                className="textLink"
-                            >
-                                API from : https://hp-api.herokuapp.com/
-                            </a>
-                        </p>
-                    </Box>
-                    <Box className="contactContainer">
-                        <h2 className="contact">Contact</h2>
-                        <p className="contactDesc">tonymigeonpro@gmail.com</p>
-                    </Box>
-                </Box>
-                <Box sx={{ width: "50%" }}>
-                    <img className="homeImage" src={castle} alt="hogwarts castle" />
+                <Typography variant="h2">Welcome to my Harry Potter Character Displayer</Typography>
+                <Box>
+                    <Button>Characters</Button>
                 </Box>
             </Box>
         </section>
